f9c358b549d8a9d4e27a7ad44a50cf9df9ef9c6b
hatyasatya <92498045+hatyasatya@users.noreply.github.com>
2026-08-31 11:59:44 +0700

    update navbar

--- resources/views/home/home_temp/index.blade.php
+++ resources/views/home/home_temp/index.blade.php
@@ -2,11 +2,13 @@
 
 @section('content')
     @include('home.home_temp.partials.hero')
+    @include('home.home_temp.partials.carousel')
+    
+    @include('home.home_temp.partials.about')
     @include('home.home_temp.partials.services')
     @include('home.home_temp.partials.portfolio')
     @include('home.home_temp.partials.process')
     @include('home.home_temp.partials.price')
     @include('home.home_temp.partials.testimonial')
     @include('home.home_temp.partials.faq')
-    @include('home.home_temp.partials.consultation')
 @endsection
--- resources/views/home/home_temp/layouts/navbar.blade.php
+++ resources/views/home/home_temp/layouts/navbar.blade.php
@@ -19,14 +19,47 @@
 
         <nav class="site-navbar__nav" id="siteNavbarNav" aria-label="Main navigation">
             <ul class="site-navbar__links">
-                <li><a href="#" class="is-active">Beranda</a></li>
+                <li><a href="#" class="is-active">Home</a></li>
                 <li><a href="#">Layanan</a></li>
                 <li><a href="#">Portfolio</a></li>
-                <li><a href="#">Paket Harga</a></li>
-                <li><a href="#">Proses</a></li>
-                <li><a href="#">Tentang Kami</a></li>
-                <li><a href="#">Blog</a></li>
-                <li><a href="#">Kontak</a></li>
+                <li><a href="#">Inovasi</a></li>
+                <li><a href="#">Informasi</a></li>
+
+                <li class="site-navbar__item--dropdown">
+                    <button type="button"
+                            class="site-navbar__dropdown-toggle"
+                            aria-expanded="false"
+                            aria-haspopup="true"
+                            aria-controls="siteNavbarDropdownMedia">
+                        Media
+                        <svg class="site-navbar__caret" viewBox="0 0 12 8" aria-hidden="true">
+                            <path d="M1 1l5 5 5-5" stroke="currentColor" stroke-width="1.6" fill="none" stroke-linecap="round" stroke-linejoin="round"/>
+                        </svg>
+                    </button>
+                    <ul class="site-navbar__dropdown" id="siteNavbarDropdownMedia">
+                        <li><a href="#">Blog</a></li>
+                        <li><a href="#">Galeri</a></li>
+                    </ul>
+                </li>
+
+                <li class="site-navbar__item--dropdown">
+                    <button type="button"
+                            class="site-navbar__dropdown-toggle"
+                            aria-expanded="false"
+                            aria-haspopup="true"
+                            aria-controls="siteNavbarDropdownTentang">
+                        Tentang Kami
+                        <svg class="site-navbar__caret" viewBox="0 0 12 8" aria-hidden="true">
+                            <path d="M1 1l5 5 5-5" stroke="currentColor" stroke-width="1.6" fill="none" stroke-linecap="round" stroke-linejoin="round"/>
+                        </svg>
+                    </button>
+                    <ul class="site-navbar__dropdown" id="siteNavbarDropdownTentang">
+                        <li><a href="#">Visi &amp; Misi</a></li>
+                        <li><a href="#">Struktur Organisasi</a></li>
+                        <li><a href="#">Team</a></li>
+                    </ul>
+                </li>
+                <li><a href="#">Kalkulator</a></li>
             </ul>
 
             <div class="site-navbar__actions">
@@ -49,5 +82,5 @@
 </header>
 
 <script>
-    
+
 </script>
